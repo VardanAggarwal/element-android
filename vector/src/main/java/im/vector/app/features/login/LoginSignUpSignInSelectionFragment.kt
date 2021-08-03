8dd0010daf8ec39403f325eafa428f44f79b9d22
vardan <vardanaggarwal@gmail.com>
2021-08-03 13:39:42 +0530

    partial changes on signin screen.

--- vector/src/main/java/im/vector/app/features/login/LoginSignUpSignInSelectionFragment.kt
+++ vector/src/main/java/im/vector/app/features/login/LoginSignUpSignInSelectionFragment.kt
@@ -52,7 +52,7 @@ class LoginSignUpSignInSelectionFragment @Inject constructor() : AbstractSSOLogi
         when (state.serverType) {
             ServerType.MatrixOrg -> {
                 views.loginSignupSigninServerIcon.setImageResource(R.drawable.ic_logo_matrix_org)
-                views.loginSignupSigninServerIcon.isVisible = true
+                views.loginSignupSigninServerIcon.isVisible = false
                 views.loginSignupSigninTitle.text = getString(R.string.login_connect_to, state.homeServerUrlFromUser.toReducedUrl())
                 views.loginSignupSigninText.text = getString(R.string.login_server_matrix_org_text)
             }
@@ -102,7 +102,7 @@ class LoginSignUpSignInSelectionFragment @Inject constructor() : AbstractSSOLogi
             }
             else             -> {
                 views.loginSignupSigninSubmit.text = getString(R.string.login_signup)
-                views.loginSignupSigninSignIn.isVisible = true
+                views.loginSignupSigninSignIn.isVisible = false
             }
         }
     }
